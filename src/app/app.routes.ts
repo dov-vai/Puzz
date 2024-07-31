207843e07fb932c95dc7aa30f26551127d9e4b75
dov-vai <160327869+dov-vai@users.noreply.github.com>
2024-07-31 23:33:56 +0300

    feat: component routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,36 @@
 import {Routes} from '@angular/router';
 import {GameComponent} from "./components/game/game.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
+import {PublicGamesComponent} from "./components/public-games/public-games.component";
+import {HostGameComponent} from "./components/host-game/host-game.component";
 
 export const routes: Routes = [
   {
     path: "",
-    title: "game",
+    title: "Home",
+    component: HomeComponent,
+    children: [
+      {
+        path: "",
+        title: "Host Game",
+        component: HostGameComponent
+      },
+      {
+        path: "public",
+        title: "Public Rooms",
+        component: PublicGamesComponent
+      }
+    ]
+  },
+  {
+    path: "play",
+    title: "Game",
     component: GameComponent
+  },
+  {
+    path: "**",
+    title: "Not Found",
+    component: PageNotFoundComponent
   }
 ];
