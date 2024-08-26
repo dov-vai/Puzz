c91fbeae43d44aaf1dcef14d3debc44dacc99995
dov-vai <160327869+dov-vai@users.noreply.github.com>
2024-08-26 22:05:51 +0300

    feat: add login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,47 @@
+import {Component, inject} from '@angular/core';
+import {AuthService} from "../../services/auth/auth.service";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  invalid: boolean = false;
+
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  get username() {
+    return this.loginForm.get('username')?.value;
+  }
+
+  get password() {
+    return this.loginForm.get('password')?.value;
+  }
+
+  login() {
+    this.invalid = false;
+    this.authService.login(this.username!, this.password!).subscribe({
+      next: (response) => console.log("logged in succesfully"),
+      error: (error) => this.invalid = true,
+    });
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
+}
