891915fcc4b84b70c67af1bd4c8a126f2b8671ec
dov-vai <160327869+dov-vai@users.noreply.github.com>
2024-09-02 18:50:44 +0300

    test(game): add tests

--- src/app/components/game/game.component.spec.ts
+++ src/app/components/game/game.component.spec.ts
@@ -1,14 +1,34 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {GameComponent} from './game.component';
+import {GameComponent, GameExtras} from './game.component';
+import {GameService} from "../../services/game/game.service";
+import {WebSocketService} from "../../services/web-socket/web-socket.service";
+import {provideRouter, Router, UrlTree} from "@angular/router";
 
 describe('GameComponent', () => {
   let component: GameComponent;
   let fixture: ComponentFixture<GameComponent>;
+  let gameServiceMock: any;
+  let webSocketServiceMock: any;
 
   beforeEach(async () => {
+    gameServiceMock = {
+      init: jasmine.createSpy('init').and.returnValue(Promise.resolve()),
+      getImageUri: jasmine.createSpy('getImageUri').and.returnValue("uri"),
+      destroy: jasmine.createSpy('destroy')
+    }
+
+    webSocketServiceMock = {
+      sendMessage: jasmine.createSpy('sendMessage'),
+    }
+
     await TestBed.configureTestingModule({
-      imports: [GameComponent]
+      imports: [GameComponent],
+      providers: [
+        {provide: GameService, useValue: gameServiceMock},
+        {provide: WebSocketService, useValue: webSocketServiceMock},
+        provideRouter([]),
+      ]
     })
       .compileComponents();
 
@@ -19,5 +39,31 @@ describe('GameComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle image preview', () => {
+    component.previewImage();
+    expect(component.showImage).toBeTrue();
+
+    component.previewImage();
+    expect(component.showImage).toBeFalse();
+  });
+
+  it('should get image URI when previewing image for the first time', () => {
+    component.previewImage();
+
+    expect(gameServiceMock.getImageUri).toHaveBeenCalled();
+    expect(component.imageUri).toBe('uri');
+    expect(component.showImage).toBeTrue();
+  });
+
+  it('should send a disconnect message on onBack()', () => {
+    component.onBack();
+    expect(webSocketServiceMock.sendMessage).toHaveBeenCalledWith({ Type: 'disconnect' });
+  });
+
+  it('should destroy the game on ngOnDestroy', () => {
+    component.ngOnDestroy();
+    expect(gameServiceMock.destroy).toHaveBeenCalled();
   });
 });
