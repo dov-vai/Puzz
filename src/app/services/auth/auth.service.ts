6af4140e9d892e5d096605571593c63309cfc484
dov-vai <160327869+dov-vai@users.noreply.github.com>
2024-09-09 22:10:57 +0300

    feat(auth-service): user info

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {BehaviorSubject, map, Observable} from "rxjs";
 import {environment} from "../../../environments/environment";
+import {UserInfo} from "./user-info";
 
 const API_URL = environment.apiUrl;
 
@@ -9,6 +10,8 @@ const API_URL = environment.apiUrl;
   providedIn: 'root'
 })
 export class AuthService {
+  private userInfoSubject = new BehaviorSubject<UserInfo | null>(null);
+  userInfo$ = this.userInfoSubject.asObservable();
 
   constructor(private http: HttpClient) {
   }
@@ -18,6 +21,23 @@ export class AuthService {
   }
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post(API_URL + "/login", {username, password});
+    return this.http.post<UserInfo>(API_URL + "/login", {username, password}).pipe(
+      map(response => this.userInfoSubject.next(response)),
+    );
+  }
+
+  tryGetUserInfo() {
+    if (this.userInfoSubject.value != null) {
+      return;
+    }
+
+    this.http.get<UserInfo>(API_URL + "/user-info").subscribe({
+      next: (response) => this.userInfoSubject.next(response),
+      error: (error) => {
+        if (error.status != 401) {
+          console.log(error);
+        }
+      }
+    });
   }
 }
